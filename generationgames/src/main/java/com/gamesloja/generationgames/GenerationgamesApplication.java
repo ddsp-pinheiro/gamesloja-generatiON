2e2b0848ee475ab95b745249c3d2e0fe0a20c19c
ddsp-pinheiro <ddsp.pinheiro@gmail.com>
2021-02-24 16:23:32 -0300

    SpringBoot model

---
+++ generationgames/src/main/java/com/gamesloja/generationgames/GenerationgamesApplication.java
@@ -0,0 +1,13 @@
+package com.gamesloja.generationgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerationgamesApplication.class, args);
+	}
+
+}
---
+++ generationgames/src/test/java/com/gamesloja/generationgames/GenerationgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamesloja.generationgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerationgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
